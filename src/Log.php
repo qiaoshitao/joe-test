f030054a65b3bfc263e87ece5f1a3c7510a130c6
qiaoshitao <qiaoshitao@chehang168.com>
2021-05-08 14:32:03 +0800

    log

--- src/Log.php
+++ src/Log.php
@@ -12,7 +12,7 @@ class Log
     public static function info($string)
     {
         $log = new Logger('aabbcc');
-        $log->pushHandler(new StreamHandler(__DIR__.'/your.log'), Logger::INFO);
+        $log->pushHandler(new StreamHandler('/Users/qiaoshitao/www/my/tests/your.log'), Logger::INFO);
         $log->info($string);
     }
 
